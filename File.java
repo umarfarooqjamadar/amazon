eeeacb9495d02103e600cf68f4fe79353e7424a7
Cloud User <ec2-user@ip-172-31-13-230.ap-south-1.compute.internal>
2020-06-20 06:59:42 +0000

    added my new file

---
+++ File.java
@@ -0,0 +1,7 @@
+public class File
+        {
+              public static void main(String[] args)
+              {
+                      System.out.println("jenkins integration with git handling by umar");
+              }
+       }
